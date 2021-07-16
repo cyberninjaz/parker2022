b9a879b56971f2c7982c51f87e64e385dcd713e3
Royalsoftwareisonline <62856825+Royalsoftwareisonline@users.noreply.github.com>
2021-07-16 09:50:10 -0400

    Finshe cod e

--- My Cool Project/CNZ_Code.java
+++ My Cool Project/CNZ_Code.java
@@ -7,9 +7,9 @@ import javax.swing.JFrame;
 import javax.swing.JTextField; 
 
 public class CNZ_Code {
-
+	
 	private static int step = 0;
-	private static String name = "Human";
+	private static String name = "User";
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("IDK");
@@ -77,6 +77,7 @@ public class CNZ_Code {
 						step = 4;
 					}
 					else if (step == 4) {
+						String message = "";
 						boolean isColor;
 						if (userInput.equalsIgnoreCase("Red")) {
 							textArea.setForeground(Color.RED);
@@ -91,46 +92,135 @@ public class CNZ_Code {
 							textArea.setForeground(Color.GREEN);
 							isColor = true;
 						} else if (userInput.equalsIgnoreCase("Blue")) {
-							textArea.type("The girl's face lights up. \"Like my dress!\" She says. She hugs you tightly, "
+							message += "The girl's face lights up. \"Like my dress!\" She says. She hugs you tightly, "
 									+ "then fades into the fog, like a distant\n"
-									+ "ancient memory\n");
+									+ "ancient memory\n";
 							textArea.setForeground(Color.BLUE);
 							isColor = true;
 						} else if (userInput.equalsIgnoreCase("Purple")) {
 							textArea.setForeground(Color.MAGENTA);
 							isColor = true;
 						} else {
-							textArea.type("\"You see " + userInput + " in the rainbow?\n"
-									+ "Out of all of these colors in the rainbow, which one do you like the most?\n");
+							message += "\"You see " + userInput + " in the rainbow?\n"
+									+ "Out my nuts itch  of all of these colors in the rainbow, which one do you like the most?\n";
 							isColor = false;
 						}
-						
+
 						if (isColor) {
-							textArea.type("Wonderful choice!" + name + " now we can begin.\n"
-									+ "Dead end \n You are at the end of a dirt road. The road goes to the east. \n"
+							message += "Wonderful choice! " + name + " now we can begin.\n"
+									+ "Dead end \n You are at the end of a dirt road. A thick fog envelopes \n"
+									+ "you. The road goes to the east. \n"
 									+ "In the distance you can see that it will eventually fork off. The \n"
 									+ "trees here are very tall royal palms, and they are spaced equidistant \n"
-									+ "from each other.");
-							
+									+ "from each other.";
+
+							textArea.type(message);
 							step = 5;
 						}
-					}
 
+					}
+					else if (step == 5) {
+						if (userInput.matches("(?).*\\beast\\b.*")) {
+							textArea.type("Dirt road \n"
+									+ "You are on the continuation of a dirt road. \n"
+									+ "There are more trees on both sides of you.  \n"
+									+ "The road continues to the west."
+									+ "You hear a voice off in the distance.");
+
+							step = 6; 
+						}
+						else { textArea.type("A thick fog envelopes the path. You cannot go that way."); }
+					}
+					else if (step == 6) {
+						if(userInput.matches("(?).*\\bwest\\b.*")) {
+							textArea.type("You enter a small clearing. An old man in red fishes in a pond. \n"
+									+ " He looks up at you. \"There is a path to the north of here.\" \n"
+									+ " He says. \"After awhile you will come to a fork in the path. \n"
+									+ " On one side there is a knight, he only tells the truth. On the \n"
+									+ " other side there is a knave, she only lies. You can ask only one \n"
+									+ " question to either, but they will only answer in YES or NO. \n"
+									+ "You will not be able to tell which is which."
+									+ "In order to pass you must figure who is knight and who is knave. \n"
+									+ "Listen to me child, you MUST ask it: \"What will be your answer if \n"
+									+ "I ask you whether the path to my left is the safe path?\" \"Ask, just \n"
+									+ "like that, no typos. Trust me child!\""
+									+ "The old man resumes fishing.");
+
+							step = 7; 
+						}
+						else { textArea.type("A thick fog envelopes the path. You cannot go that way."); }
+					}
+					else if (step == 7) {
+						if(userInput.matches("(?).*\\bnorth\\b.*")) {
+							textArea.type("After a few hours of walking, you come upon a fork in the path. \n"
+									+ "You decide to rest yourself here. \"HALT!\" A voice commands. \n"
+									+ "You look up to see two armor clad guards. You can't tell which \n"
+									+ " is which. You suddenly remember what the Old Man in Red told \n"
+									+ " you to do. You turn to one of the guards. \n"
+									+ "What question do you ask it? \n");
+							step = 8; 
+						}
+						else { textArea.type("A thick fog envelopes the path. You cannot go that way."); }
+					}
+					else if (step == 8) {
+						if(userInput.equals("What will be your answer if I ask you whether the path to my left is the safe path?")) {
+							textArea.type("The guard looks at you. \"YES\" It says, in a robotic tone. \n"
+									+ "Do you trust the guard and take the left path?");
+							step = 9;
+						}
+						else { textArea.type("That's not the right question!"); }
+						// Make it so it reads input again? //
+					}
+					else if (step == 9) {
+						if(userInput.matches("(?).*\\bYes\\b.*")) {
+							textArea.type("The guard lets you pass. You enter through the gate into a city \n"
+									+ " full of riches, and prosper till the end of your days.");
+							step = 10; 
+
+						}
+						else { if(userInput.matches("(?)).*\\bno\\b.*")) {
+							textArea.type("You decide to take the right path. You walk down it, but before \n "
+									+ " you can enter the gate, you feel a sharp pain. You look down to \n"
+									+ " see a knife protruding from your back through your stomach. \n"
+									+ "YOU HAVE DIED \n"
+									+ "              \n"
+									+ "Congratulations" + name + "you've almost completed the evaluation! \n"
+									+ "Just one more thing: \n"
+									+ "Do you trust me? \n"
+									+ "Am I a knight, or a knave? \n"
+									+ "Do you trust... the company, " + name + "?");
+
+							step = 10; 
+						}
+						}
+
+					} 
+					else if (step == 10) { 
+						if(userInput.matches("(?)).*\\bno\\b.*")) {
+							textArea.type("Interesting.... /n"
+									+ "Please email [INSERT EMAIL HERE] with your evaluation results. Have a good day " + name);
+						}
+
+					} else {
+						textArea.type("Correct! /n"
+								+ "Please email [INSERT EMAIL HERE] with your evaluation results. Have a nice day!");
+					}
+				}
+			}
+
+				@Override 
+				public void keyReleased(KeyEvent e) {
 				}
 
-			}
 
-			@Override 
-			public void keyReleased(KeyEvent e) {
-			}
 
-		});
+			}); 
 
 		window.setSize(600, 400);
 		window.setLocationRelativeTo(null); // window in center
 		window.setVisible(true);
 
 		textArea.type("Hello\n");
-	}
+		}
 
-}
+	}
